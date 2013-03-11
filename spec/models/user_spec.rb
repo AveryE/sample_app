60c3a1395677b0410aaab3e28f2f0a5347f9ac7b
rubyspring <elavery1@madisoncollege.edu>
2013-03-11 14:17:51 -0500

    working on chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
 require 'spec_helper'
 
 describe User do
@@ -25,6 +26,9 @@ before do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+
+  it { should respond_to(:remember_token) }
+
   it { should respond_to(:authenticate)}
   it { should be_valid }
 
@@ -127,6 +131,14 @@ end
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+    
+    
+
+  end
+    
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
   
