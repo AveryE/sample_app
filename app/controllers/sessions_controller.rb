4d2498a9a79909596f644a593665324eedfa10af
rubyspring <elavery1@madisoncollege.edu>
2013-03-13 13:59:58 -0500

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,13 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
+
+   def destroy
+    sign_out
+    redirect_to root_url
   end
+
+  
 end
 
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,6 +41,9 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+      
+      
+      
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -53,6 +56,7 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
